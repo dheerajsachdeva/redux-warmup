1567c0e9c319c455f6a0f78a47cbe6ee1e99c470
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-04-04 20:30:18 +0530

    update all files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Navbar from "./components/Navbar.js";
 import CartContainer from "./components/CartContainer.js";
 import { useDispatch, useSelector} from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice.js";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice.js";
 import { useEffect } from "react";
 import React from "react";
 import Modal from "./components/Modal.js";
 
 function App() {
 
-  const {cartItems} = useSelector((store) => store.cart)
+  const {cartItems, isLoading} = useSelector((store) => store.cart)
   const {isOpen} = useSelector((store) => store.modal)
   
   const dispatch = useDispatch();
@@ -16,6 +16,16 @@ function App() {
   useEffect (() =>{
       dispatch(calculateTotals())
   }, [cartItems])
+
+  useEffect (() =>{
+    dispatch(getCartItems())
+}, [])
+
+if (isLoading){
+  return <div className="loading">
+    <h1>Loading...</h1>
+  </div>
+}
 
   return(<main>
     { isOpen && <Modal />}
